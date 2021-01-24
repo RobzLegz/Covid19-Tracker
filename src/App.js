1bc7744f57d0ffaf8b87347cfd9c13ae82dffc9e
Robzlegz <robzlegz@gmail.com>
2021-01-24 19:52:25 +0200

    adding media querries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core'; 
 import './App.css';
 import InfoCard from './InfoCard';
+import Map from "./Map";
 
 function App() {
 
@@ -29,24 +30,33 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="app-header">
-        <h1>COVID 19 TRACKER</h1>
-        <FormControl className="app-dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country} >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-          
-        </FormControl>
+    <div className="app">
+      <div className="app-left">
+        <div className="app-header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app-dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country} >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+            
+          </FormControl>
+        </div>
+        <div className="app-cards">
+          <InfoCard title="Coronavirus Cases" cases={123} total={2000}/>
+          <InfoCard title="Recovered" />
+          <InfoCard title="Deaths" />
+        </div>      
+        <Map />
       </div>
-      <div className="app-cards">
-        <InfoCard title="Coronavirus Cases" cases={123} total={2000}/>
-        <InfoCard title="Recovered" />
-        <InfoCard title="Deaths" />
-      </div>      
+      <Card className="app-right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
