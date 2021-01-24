df9fa00faf64688cfdfdb4e9a425ebc47af33fca
Robzlegz <robzlegz@gmail.com>
2021-01-24 18:49:14 +0200

    adding dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import React from 'react';
+import {MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID 19 TRACKER</h1>
+      <FormControl className="app-dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+          <MenuItem value="worldwide">Option2</MenuItem>
+          <MenuItem value="worldwide">Option3</MenuItem>
+          <MenuItem value="worldwide">Option4</MenuItem>
+        </Select>
+        
+      </FormControl>
     </div>
   );
 }
