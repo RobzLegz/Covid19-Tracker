1f2d44c86e6257528d905040e20f7221802169ba
Robzlegz <robzlegz@gmail.com>
2021-01-24 19:27:58 +0200

    creating new element

--- src/App.js
+++ src/App.js
@@ -7,27 +7,32 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
 
-    useEffect(() => {
-      const getCountryData = async () => {
-        await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => ({
-            name: country.country,
-            value: country.countryInfo.iso2
-          }));
-          setCountries(countries);
-        });
-      };
-      getCountryData();
-    }, []);
+  useEffect(() => {
+    const getCountryData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2
+        }));
+        setCountries(countries);
+      });
+    };
+    getCountryData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
 
   return (
     <div className="App">
       <div className="app-header">
         <h1>COVID 19 TRACKER</h1>
         <FormControl className="app-dropdown">
-          <Select variant="outlined" value={country} >
+          <Select variant="outlined" onChange={onCountryChange} value={country} >
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -36,7 +41,9 @@ function App() {
           
         </FormControl>
       </div>
-      
+      <div className="app-cards">
+        
+      </div>      
     </div>
   );
 }
---
+++ src/InfoCard.js
@@ -0,0 +1,14 @@
+import { Card } from '@material-ui/core'
+import React from 'react';
+import {Card, CardContent, Typography} from "@material-ui/core";
+function InfoCard({title, cases, total}) {
+    return (
+        <Card>
+            <CardContent>
+                
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoCard
